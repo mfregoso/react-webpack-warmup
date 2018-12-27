db9f398274e64706fdd06bc91090777e573610f2
mfregoso <mfregoso@users.noreply.github.com>
2018-12-26 17:26:38 -0800

    specific output dir

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-const msg = "hello from node";
-console.log(msg);
+const msgs = [
+  "hello from 1node",
+  "hello from 2node",
+  "hello from 3node",
+  "hello from 4node"
+];
+
+for (let msg of msgs) {
+  console.log(msg);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   mode: "development", // or production
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js"
+    filename: "bundle.js",
+    path: path.join(__dirname, "build")
   }
 };
