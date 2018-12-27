edd6e9d2016faf883cbff348c42e6d578b487bfd
mfregoso <mfregoso@users.noreply.github.com>
2018-12-26 20:45:09 -0800

    add css/style loader to webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Welcome from "./components/Welcome";
+import "./styles/custom.css";
 
 ReactDOM.render(
   <Welcome title="Webpack Warmup" />,
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,17 @@ module.exports = {
             presets: ["@babel/preset-env", "@babel/preset-react"]
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
       }
     ]
   }
